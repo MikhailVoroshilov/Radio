fbe6e920b071b9f9be186f68983040a923b5e46d
Mikhail Voroshilov <m.s.voroshilovv@gmail.com>
2022-02-02 10:28:37 +0800

    100% branch

--- src/test/java/ru/netology/RadioTest.java
+++ src/test/java/ru/netology/RadioTest.java
@@ -42,7 +42,31 @@ class RadioTest {
     }
 
     @Test
+    void shouldNextRadioStationMax() {
+        radio.setRadioStation(9);
+        radio.nextRadioStation();
+
+
+        int expected = 0;
+        int actual = radio.getRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void shouldPrevRadioStation() {
+        radio.setRadioStation(1);
+        radio.prevRadioStation();
+
+
+        int expected = 0;
+        int actual = radio.getRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldPrevRadioStationMin() {
         radio.setRadioStation(0);
         radio.prevRadioStation();
 
@@ -55,9 +79,19 @@ class RadioTest {
 
     @Test
     void shouldSetRadioStation() {
-        radio.setRadioStation(9);
+        radio.setRadioStation(-1);
 
-        int expected = 9;
+        int expected = 0;
+        int actual = radio.getRadioStation();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldSetRadioStationNull() {
+        radio.setRadioStation(10);
+
+        int expected = 0;
         int actual = radio.getRadioStation();
 
         assertEquals(expected, actual);
@@ -66,6 +100,16 @@ class RadioTest {
     @Test
     void shouldValue() {
         radio.setVolume(11);
+
+        int expected = 0;
+        int actual = radio.getVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldValueMin() {
+        radio.setVolume(-1);
 
         int expected = 0;
         int actual = radio.getVolume();
@@ -85,8 +129,29 @@ class RadioTest {
     }
 
     @Test
+    void shouldLouderMin() {
+        radio.setVolume(0);
+        radio.louder();
+
+        int expected = 1;
+        int actual = radio.getVolume();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
     void shouldQuiet() {
-        radio.setVolume(1);
+        radio.setVolume(10);
+        radio.quiet();
+
+        int expected = 9;
+
+        assertEquals(expected, radio.getVolume());
+    }
+
+    @Test
+    void shouldQuietMin() {
+        radio.setVolume(0);
         radio.quiet();
 
         int expected = 0;
